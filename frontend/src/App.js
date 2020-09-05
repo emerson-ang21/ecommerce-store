bd361495fad33623dfc1813158c8f92cddd3f0dd
emerson-ang21 <emersonang91@gmail.com>
2020-09-05 02:00:14 -0700

    Manage state with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
-import {BrowserRouter, Route, Link } from 'react-router-dom';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 
@@ -13,46 +13,48 @@ function App() {
     document.querySelector(".sidebar").classList.remove("open")
   }
   return (
-  <BrowserRouter>
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>
-            &#9776;
-            </button>
-          <Link to="/">amazona</Link>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+        </button>
+            <Link to="/" >amazona</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
 
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
 
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/" exact={true} component={HomeScreen} />
-          
-        </div>
-      </main>
-      <footer className="footer">
-        All right reserved.
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+
+
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
     </footer>
-    </div>
-  </BrowserRouter>
+      </div>
+    </BrowserRouter>
   );
 }
 
