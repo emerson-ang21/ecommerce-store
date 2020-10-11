ea8d46cc6930572e28857e813d831ae8ae7ef144
Javie Onglao <starpogi@gmail.com>
2020-10-10 21:22:47 -0700

    CORS

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,13 @@ mongoose.connect(mongodbUrl, {
 
 
 const app = express();
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
