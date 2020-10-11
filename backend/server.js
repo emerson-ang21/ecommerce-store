63c24d46d0ca4c724834c6235b0be5b91c9ebd82
Javie Onglao <starpogi@gmail.com>
2020-10-10 21:31:18 -0700

    CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose.connect(mongodbUrl, {
 
 const app = express();
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://phfarms.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
